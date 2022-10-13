435d6120c433fe8fb8069f417d27473d2ce03b0b
devMauriciojunior <110423039+devMauriciojunior@users.noreply.github.com>
2022-10-13 14:21:05 -0300

    inicio de condições

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 110
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 75) {  //Condição simples
+   console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de seguraça!')
