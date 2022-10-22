962fdf2c075ea329d2717fe2f2bb82f0dce0fac4
devMauriciojunior <110423039+devMauriciojunior@users.noreply.github.com>
2022-10-22 10:35:50 -0300

    hora em tempo real

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -3,12 +3,20 @@ function carregar () {
    const img = window.document.getElementById('imagem')
    const data = new Date()
    const hora = data.getHours()
-   msg.innerHTML = `Agora são ${hora} horas.`
+   const minuto = data.getMinutes()
+   const segun = data.getSeconds()
+   const relogio = data.toLocaleTimeString()
+   //msg.innerHTML = `Agora são ${hora}:${minuto}:${segun} horas.`
+   msg.innerHTML = `Agora são ${relogio}.`
 
+   if (minuto < 10) {
+      minuto = "0" + minuto
+   }
+   
    if (hora >= 0 && hora < 12) {
       // Bom dia
       img.src = 'img/manha.jpg'
-      document.body.style.background = '#A7D9D9'
+      document.body.style.background = '#6aacac'
    } else if (hora >= 12 && hora <= 18) {
       // Boa tarde
       img.src = 'img/tarde.jpg'
@@ -16,7 +24,8 @@ function carregar () {
    } else {
       // Boa noite
       img.src = 'img/noite.jpg'
-      document.body.style.background = '#0367A6'
+      document.body.style.background = '#02588d'
    }
-}
 
+   setInterval (carregar, 1000)
+}
