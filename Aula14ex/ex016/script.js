43034c2658a2eadfb3a4175b1631c10d8642f3da
devMauriciojunior <110423039+devMauriciojunior@users.noreply.github.com>
2022-10-24 17:01:39 -0300

    Exercicios

---
+++ Aula14ex/ex016/script.js
@@ -0,0 +1,38 @@
+function contar() {
+   let ini = document.getElementById('txti')
+   let fim = document.getElementById('txtf')
+   let pas = document.getElementById('txtp')
+   let res = document.getElementById('res')
+
+   if (ini.value.length == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+      alert ('Faltam dados')
+   } else {
+      res.innerHTML = 'Contando.. <br>'
+
+      let i = Number(ini.value)
+      let f = Number(fim.value)
+      let p = Number(pas.value)
+
+      if (p <= 0) {
+         alert('Passo inválido, considerando passo 1')
+         p = 1
+      }
+
+      if (i < f) {
+         // Contagem crescente
+         let c = i
+         while (c <= f) {
+            res.innerHTML += `${c} \u{27A1}`
+            c += p
+         }
+         /* for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{27A1} `
+         }*/
+      } else {
+         // Contagem regressiva
+         for (let c = i; c >= f; c -= p) {
+            res.innerHTML = `${c} \u{27A1}`
+         }
+      }
+   }
+}
