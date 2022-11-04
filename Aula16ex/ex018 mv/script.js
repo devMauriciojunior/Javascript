a57dbd92587e55dd32f8b8552bdb7fd57169dafa
devMauriciojunior <110423039+devMauriciojunior@users.noreply.github.com>
2022-11-04 18:28:01 -0300

    finalizado

--- Aula16ex/ex018 mv/script.js
+++ Aula16ex/ex018 mv/script.js
@@ -1,24 +1,66 @@
-let inpn = document.getElementById('txtn')
+let num = document.getElementById('txtn')
 let res = document.getElementById('res')
 let textos = document.getElementById('textos')
 let valores = []
 
-function adicionar() {
-   if (inpn.value == 0) {
-      alert('DIGITE UM NÚMERO')
-   } else if (inpn.value >= 101) {
-      alert ('APENAS NÚMEROS DE 1 ATÉ 100')
+function isNumero (n) {
+   if (Number(n) >= 1 && Number(n) <= 100) {
+      return true
    } else {
-      let valor = (inpn.value)
-      let elemento = document.createElement('option')
-      elemento.text = `Número ${valor} adicionado`
-      res.appendChild(elemento)
+      return false
    }
 }
 
+function inList (n, l) {
+   if (l.indexOf(Number(n)) != -1) {
+      return true
+   } else {
+      return false
+   }
+} 
+
+function adicionar() {
+   if (isNumero(num.value) && !inList(num.value, valores)) {
+      valores.push(Number(num.value))
+      let item = document.createElement('option')
+      item.text = `Número ${num.value} adicionado`
+      res.appendChild(item)
+      textos.innerHTML = ''
+   } else {
+      alert('NÚMERO INVÁLIDO OU JÁ ENCONTRADO NA LISTA')
+   }
+
+   num.value = ''
+   num.focus()
+}
+
 function finalizar() {
-   if (inpn.value == 0) {
-      alert('DIGITE UM NÚMERO')
+   if (valores.length == 0) {
+      alert('ADICIONE VALORES')
+   } else {
+      let total = valores.length
+      let maior = valores[0]
+      let menor = valores[0]
+      let soma = 0
+      let media = 0 
+
+      for (let pos in valores) {
+         soma += valores[pos]
+
+         if (valores[pos] > maior) {
+            maior = valores[pos]
+         }
+         if (valores[pos] < menor) {
+            menor = valores[pos]
+         }
+      }
+      media = soma / total
+
+      textos.innerHTML += `<p>Temos ${total} números cadastrados.</p>`
+      textos.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+      textos.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+      textos.innerHTML += `<p>A soma dos valores é igual a ${soma}.</p>`
+      textos.innerHTML += `<p>A média do valores é ${media.toFixed(2)}.</p>`
    }
 }
 
